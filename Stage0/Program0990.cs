5eebf23217d2eeeed3ba8cdcc497ef49e6529760
Noa <noaleamarciano@gmail.com>
2023-09-18 14:22:29 +0300

    Initial code + welcome functions

---
+++ Stage0/Program0990.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0990();
+            Welcome5487();
+            Console.ReadKey();
+        }
+        static partial void Welcome5487();
+        private static void Welcome0990()
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
