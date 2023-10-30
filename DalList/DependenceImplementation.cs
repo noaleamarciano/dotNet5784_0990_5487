c035ed66184d5e1949d5be2dbaf252a98707510f
EfratYi <ey32423632@gmail.com>
2023-10-30 21:59:27 +0200

    I created files for each class entity that will implement the methods

---
+++ DalList/DependenceImplementation.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Dal;
+using DalApi;
+using DO;
+using System.Collections.Generic;
+
+public class DependenceImplementation : IDependence
+{
+    public int Create(Dependence item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependence? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Dependence> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Dependence item)
+    {
+        throw new NotImplementedException();
+    }
+}
