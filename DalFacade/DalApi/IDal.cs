60232b4ccbe42acc09a2f410ff04ba251f8c0103
214785487 <214785487@LAB41PC2>
2023-11-14 16:06:10 +0200

    Arrange the project - stage2

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi;
+
+internal interface IDal
+{
+    IEngineer Engineer { get; }
+    ITask Task { get; }
+    IDependence Dependence { get; }
+}
